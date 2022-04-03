ba82c641349ba51103487ed201341ab76b0c05ce
Vladimirs <vladimirribakov123@mail.ru>
2022-04-03 20:24:12 +0100

    populated table with some data

--- public/bicycles.php
+++ public/bicycles.php
@@ -5,41 +5,57 @@
 
 <div id="main">
 
-  <div id="page">
-    <div class="intro">
-      <img class="inset" src="<?php echo url_for('/images/AdobeStock_55807979_thumb.jpeg') ?>" />
-      <h2>Our Inventory of Used Bicycles</h2>
-      <p>Choose the bike you love.</p>
-      <p>We will deliver it to your door and let you try it before you buy it.</p>
+    <div id="page">
+        <div class="intro">
+            <img class="inset" src="<?php echo url_for('/images/AdobeStock_55807979_thumb.jpeg') ?>" />
+            <h2>Our Inventory of Used Bicycles</h2>
+            <p>Choose the bike you love.</p>
+            <p>We will deliver it to your door and let you try it before you buy it.</p>
+        </div>
+
+        <table id="inventory">
+            <tr>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Year</th>
+                <th>Category</th>
+                <th>Gender</th>
+                <th>Color</th>
+                <th>Weight</th>
+                <th>Condition</th>
+                <th>Price</th>
+            </tr>
+
+            <?php 
+      
+        $args = [
+          'brand' => 'Dremb',
+          'model' => 'Flesx09',
+          'year' => 2020,
+          'category' => 'Special',
+          'gender' => 'Mens',
+          'color' => 'white',
+          'weight_kg' => 12.0,
+          'price' => '233.99',
+        ];
+        $bike = new Bicycle($args);
+      
+      ?>
+            
+            <tr>
+                <td><?php echo h($bike->brand); ?></td>
+                <td><?php echo h($bike->model); ?></td>
+                <td><?php echo h($bike->year); ?></td>
+                <td><?php echo h($bike->category); ?></td>
+                <td><?php echo h($bike->gender); ?></td>
+                <td><?php echo h($bike->color); ?></td>
+                <td><?php echo h($bike->weight_kg()) . ' / ' . h($bike->weight_lbs()); ?></td>
+                <td><?php echo h($bike->condition()); ?></td>
+                <td>$<?php  echo h($bike->price); ?></td>
+            </tr>
+            
+        </table>
     </div>
-
-    <table id="inventory">
-      <tr>
-        <th>Brand</th>
-        <th>Model</th>
-        <th>Year</th>
-        <th>Category</th>
-        <th>Gender</th>
-        <th>Color</th>
-        <th>Weight</th>
-        <th>Condition</th>
-        <th>Price</th>
-      </tr>
-
-      <tr>
-        <td>Brand</td>
-        <td>Model</td>
-        <td>Year</td>
-        <td>Category</td>
-        <td>Gender</td>
-        <td>Color</td>
-        <td>Weight</td>
-        <td>Condition</td>
-        <td>Price</td>
-      </tr>
-
-    </table>
-  </div>
 
 </div>
 
