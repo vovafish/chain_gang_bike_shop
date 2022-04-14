88080dcdbb131f8d341d773e73d545637a1ba489
Vladimirs <vladimirribakov123@mail.ru>
2022-04-14 17:40:40 +0100

    improved the session class

--- private/classes/session.class.php
+++ private/classes/session.class.php
@@ -2,6 +2,10 @@
 
 class Session {
     private $admin_id;
+    public $username;
+    private $last_login;
+
+    public const MAX_LOGIN_AGE = 60 * 60 * 24; //1 day
 
     public function __construct() {
         session_start();
@@ -11,18 +15,22 @@ class Session {
         if($admin) {
             //prevent session fixation attack
             session_regenerate_id();
-            $_SESSION['admin_id'] = $admin->id;
-            $this->admin_id = $admin->id;
+            $this->admin_id = $_SESSION['admin_id'] = $admin->id;
+            $this->username = $_SESSION['username'] = $admin->username;
+            $this->last_login = $_SESSION['last_login'] = time();
         }
         return true;
     }
 
     public function is_logged_in() {
-        return isset($this->admin_id);
+        //return isset($this->admin_id);
+        return isset($this->admin_id) && $this->last_login_is_recent();
     }
 
     public function logout() {
         unset($_SESSION['admin_id']);
+        unset($_SESSION['username']);
+        unset($_SESSION['last_login']);
         unset($this->admin->id);
         return true;
     }
@@ -30,6 +38,18 @@ class Session {
     private function check_stored_login() {
         if(isset($_SESSION['admin_id'])) {
             $this->admin_id = $_SESSION['admin_id'];
+            $this->username = $_SESSION['username'];
+            $this->last_login = $_SESSION['last_login'];
+        }
+    }
+
+    private function last_login_is_recent() {
+        if(!isset($this->last_login)) {
+            return false;
+        } elseif (($this->last_login + self::MAX_LOGIN_AGE) < time()) {
+            return false;
+        } else {
+            return true;
         }
     }
 }
--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -20,6 +20,7 @@
     <navigation>
         <ul>
             <?php if($session->is_logged_in()) { ?>
+            <li>User: <?php echo $session->username; ?></li>
             <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
             <li><a href="<?php echo url_for('/staff/logout.php'); ?>">Logout</a></li>
             <?php } ?>
